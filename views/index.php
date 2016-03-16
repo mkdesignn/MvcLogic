276711e1d43875390ca10592f2564dedff546b5d
mkdesign82 <mkdesign82@yahoo.com>
2016-03-16 22:59:20 +0330

    change the feature one

--- views/index.php
+++ views/index.php
@@ -1,1 +1,10 @@
-this is feature one
+<p style='color:red'>
+	this is red text which is located in your index page
+</p>
+<p style='color:yellow'>
+	im okay ty
+<p>
+<p style='color:red'>
+im cool
+</p>
+but this is feature one
