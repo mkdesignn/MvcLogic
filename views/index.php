11b6b15f7eece07606b2bbcd206085b0f2b260a3
mkdesign82 <mkdesign82@yahoo.com>
2016-03-16 22:52:42 +0330

    post of master

--- views/index.php
+++ views/index.php
@@ -1,1 +1,9 @@
-this is feature one
+<p style='color:red'>
+	this is red text which is located in your index page
+</p>
+<p style='color:yellow'>
+	im okay ty
+<p>
+<p style='color:red'>
+im cool
+</p>
